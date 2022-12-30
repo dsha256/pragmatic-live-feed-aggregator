629a3f02b6a2c9213736189b91abedbfe7127c9b
David Shainidze <56347073+dsha256@users.noreply.github.com>
2022-12-30 19:21:09 +0400

    rest: add the get endpoint to get all the data (#4)
    * db: implement data access layer
    
    * redis: add cache layer in docker-compose.yml
    
    * dto: create pragamtic table dto
    
    * env: add env vars loading functioanlity
    
    * git: update .gitignore
    
    * build: update dependencies
    
    * config: move env.go to config package
    
    * config: update env config
    
    * ws: setup synchronized concurrent clients
    
    * rest: add the get endpoint to get all the data (#3)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"github.com/dsha256/pragmatic-live-feed-aggregator/internal/config"
 	"github.com/dsha256/pragmatic-live-feed-aggregator/internal/repo"
+	"github.com/dsha256/pragmatic-live-feed-aggregator/internal/server"
 	"github.com/dsha256/pragmatic-live-feed-aggregator/internal/ws"
 	"github.com/go-redis/redis/v8"
+	"log"
+	"net/http"
 )
 
 func main() {
@@ -21,10 +24,18 @@ func main() {
 	})
 	redisRepo := repo.NewRedisRepository(redisClient)
 
+	httpServer := server.NewHTTP(&redisRepo)
+	// TODO: Get port from env vars
+	err := http.ListenAndServe(":8080", httpServer)
+	if err != nil {
+		log.Printf("can't run the server on port 8080: %v", err)
+	}
+
 	wsURL := env.PragmaticFeedWsURL
 	casinoID := env.GetCasinoID()
 	tableIDs := env.GetTableIDs()
 	currencyIDs := env.GetCurrencyIDs()
 	wsClient := ws.NewClient(ctx, &redisRepo, wsURL, casinoID, tableIDs, currencyIDs)
+	//_ = wsClient
 	wsClient.StartClients()
 }
